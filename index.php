de83c83d2644fb30321129fbd9ccb84f209d7e99
vu <nam>
2018-12-14 08:14:35 +0700

    add code

---
+++ index.php
@@ -0,0 +1,70 @@
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>
+.left{float:left;padding-left: 20px;}
+.content-tab{display: none;}
+.content-tab.first{display: block;}
+</style>
+<div class="left tab-header" tab="1">page1</div>
+<div class="left tab-header" tab="2">page2</div>
+<div class="left tab-header" tab="3">page3</div>
+<div class="left tab-header" tab="4">page4</div>
+<div class="left tab-header" tab="5">page5</div>
+<div style="clear: left;"></div>
+<div class="content-tab first" parent-tab="1">
+  <div class="col-md-6" row="1">
+     <div class="conten-tab-text">test-1-1</div>
+     <div>
+      <input type="checkbox" value="1" />
+      <input type="checkbox" value="2" />
+      <input type="checkbox" value="3" />
+      <input type="checkbox" value="4" />
+      <input type="checkbox" value="5" />
+      <input type="checkbox" value="6" />
+     </div>
+  </div>
+  <div class="col-md-6" row="2">
+     <div class="conten-tab-text">test-1-2</div>
+     <div>
+      <input type="checkbox" value="1" />
+      <input type="checkbox" value="2" />
+      <input type="checkbox" value="3" />
+      <input type="checkbox" value="4" />
+      <input type="checkbox" value="5" />
+      <input type="checkbox" value="6" />
+     </div>
+  </div>
+</div>
+
+<div class="content-tab" parent-tab="2">
+  <div class="col-md-6" row="1">
+     <div class="conten-tab-text">test-2-1</div>
+     <div>
+      <input type="checkbox" value="1" />
+      <input type="checkbox" value="2" />
+      <input type="checkbox" value="3" />
+      <input type="checkbox" value="4" />
+      <input type="checkbox" value="5" />
+      <input type="checkbox" value="6" />
+     </div>
+  </div>
+  <div class="col-md-6" row="2">
+     <div class="conten-tab-text">test-2-2</div>
+     <div>
+      <input type="checkbox" value="1" />
+      <input type="checkbox" value="2" />
+      <input type="checkbox" value="3" />
+      <input type="checkbox" value="4" />
+      <input type="checkbox" value="5" />
+      <input type="checkbox" value="6" />
+     </div>
+  </div>
+</div>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<script src="js/init.js"></script>
+
